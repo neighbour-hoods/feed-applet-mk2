865046a40faa1a9244f247abc4ddd0091c44d583
Nicholas Stebbings <nickolarse@riseup.net>
2023-10-12 15:43:54 +1300

    update total like render styles

--- ui/src/sensemaker/widgets/total-likes-dimension-display.ts
+++ ui/src/sensemaker/widgets/total-likes-dimension-display.ts
@@ -1,5 +1,5 @@
 import { css, html } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { property } from 'lit/decorators.js';
 import { DisplayDimensionWidget } from '@neighbourhoods/client';
 import { Assessment, RangeValueInteger } from '@neighbourhoods/client';
 
@@ -10,8 +10,11 @@ export class TotalLikesDimensionAssessment extends DisplayDimensionWidget {
 
     render() {
         return html`
-                    <div>
-                        (${this.assessment ? (this.assessment.value as RangeValueInteger).Integer : 0})
+                    <div class="display-box-wrapper">
+                        <div class="display-box">
+                            <span>❤️</span>
+                            <span>${this.assessment ? (this.assessment.value as RangeValueInteger).Integer : 0}</span>
+                        </div>
                     </div>
                 `
     }
@@ -20,9 +23,28 @@ export class TotalLikesDimensionAssessment extends DisplayDimensionWidget {
         }
     }
     static styles = css`
-        .heat-scale {
+        .display-box {
+            background-color: rgb(50, 43, 55);
+            width: auto;
+            height: 32px;
             display: flex;
-            flex-direction: row;
+            justify-content: center;
+            align-items: center;
+            font-size: 16px;
+            color: var(--nh-theme-fg-default);
+            border-radius: calc(1px * var(--nh-radii-base));
+            min-width: 32px;
+            gap: calc(1px * var(--nh-spacing-sm));
+        }
+        
+        .preview-container .display-box span {
+            margin: 0 4px;
+        }
+    
+        .display-box-wrapper {
+            display: grid;
+            align-items: center;
+            justify-content: center;
         }
     `
 }
