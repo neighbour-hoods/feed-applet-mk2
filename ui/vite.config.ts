b31c66b39f2f45b439563f19ed5a179752a28ea3
Nicholas Stebbings <nickolarse@riseup.net>
2023-10-12 13:00:33 +1300

    update manifests for pnpm/packaging

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,22 +1,22 @@
-import { defineConfig } from 'vite';
-import checker from 'vite-plugin-checker';
+import { defineConfig, loadEnv } from 'vite'
+
 export default defineConfig(({ mode }) => {
   const isProduction = mode === 'production'
 
   return isProduction ? {
-    // plugins: [
-    //   {
-    //     name: 'copy-assets',
-    //     apply: 'build',
-    //     generateBundle() {
-    //       this.emitFile({
-    //         type: 'asset',
-    //         fileName: 'icon.png',
-    //         source: require('fs').readFileSync('icon.png'),
-    //       });
-    //     },
-    //   },
-    // ],
+    plugins: [
+      {
+        name: 'copy-assets',
+        apply: 'build',
+        generateBundle() {
+          this.emitFile({
+            type: 'asset',
+            fileName: 'icon.png',
+            source: require('fs').readFileSync('icon.png'),
+          });
+        },
+      },
+    ],
     build: {
       sourcemap: true,
       lib: {
