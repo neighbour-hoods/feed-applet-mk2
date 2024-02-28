829c8da605a6f02d24da8d6de25e923dea0bafc2
Nicholas Stebbings <nickolarse@riseup.net>
2024-02-28 14:47:36 +1300

    add widgets to applet

--- ui/src/applet-index.ts
+++ ui/src/applet-index.ts
@@ -4,10 +4,11 @@ import {
 
 import { FeedApplet } from "./feed-app";
 import { appletConfig } from "./appletConfig";
-import { LikeDimensionAssessment, TotalLikesDimensionAssessment } from "./sensemaker/widgets";
+import { HeatDimensionAssessment, LikeDimensionAssessment, TotalLikesDimensionAssessment } from "./sensemaker/widgets";
 
 import "./feed/components/post-display-wrapper";
 import { PostDisplayWrapper } from "./feed/components/post-display-wrapper";
+import { AverageHeatDimensionDisplay } from "./sensemaker/widgets/average-heat-dimension-display";
 
 const applet: NeighbourhoodApplet = {
   appletConfig: appletConfig,
@@ -27,6 +28,18 @@ const applet: NeighbourhoodApplet = {
     importanceOutput: {
       name: "Total Likes Display",
       component: TotalLikesDimensionAssessment,
+      rangeKind: { Integer: { min: 0, max: 4294967295 }},
+      kind: 'output'
+    },
+    heatAssessment: {
+      name: "Heat",
+      component: HeatDimensionAssessment,
+      rangeKind: { Integer: {min: 0, max: 4}},
+      kind: 'input'
+    },
+    heatOuput: {
+      name: "Average Heat",
+      component: AverageHeatDimensionDisplay,
       rangeKind: { Integer: { min: 0, max: 4294967295 }},
       kind: 'output'
     },
